c6af32fb3bb354b3e15b2046231bafecd663c134
bobumkin <112267231+bobumkin@users.noreply.github.com>
2024-03-24 11:15:32 +0500

    Update WinMain.cpp
    The window stopped closing

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,7 +1,17 @@
 #include <Windows.h>
-#include <string>
 
-int wWinMain(
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(1);
+        break;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
     _In_ HINSTANCE hInstance,
     _In_ HINSTANCE hPrevInstance,
     _In_ LPWSTR lpCmdLine,
@@ -11,7 +21,7 @@ int wWinMain(
     WNDCLASSEX wc = { 0 };
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -26,10 +36,20 @@ int wWinMain(
         0, pClassName,
         L"Happy Hard Windows",
         WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        200, 200, 650, 480,
+        200, 200, 1920, 1080,
         nullptr, nullptr, hInstance, nullptr
     );
     ShowWindow(hWnd, SW_SHOW);
-    while (true);
-    return 0;
+    MSG msg;
+    BOOL gResult;
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    if (gResult == -1) {
+        return -1;
+    }
+    else {
+        return msg.wParam;
+    }
 }
