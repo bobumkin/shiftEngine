4c0c42fe9802bee5c8c733cf0b3ac8f9a65f4411
bobumkin <112267231+bobumkin@users.noreply.github.com>
2024-04-01 23:07:30 +0500

    Update Window.h
    A new exception class has been added

--- Window.h
+++ Window.h
@@ -1,8 +1,22 @@
 #pragma once
-#include "ChiliWin.h"
+#include "EasyWin.h"
+#include "CustomException.h"
 
 class Window
 {
+public:
+	class Exception : public CustomException
+	{
+	public:
+		Exception(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		virtual const char* GetType() const noexcept;
+		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+	private:
+		HRESULT hr;
+	};
 private:
 	class WindowClass
 	{
