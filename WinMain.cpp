141eaf3e059ea54a4433080f0b8b884b3b685606
bobumkin <112267231+bobumkin@users.noreply.github.com>
2024-04-02 23:55:49 +0500

    Update WinMain.cpp
    Add exception

--- WinMain.cpp
+++ WinMain.cpp
@@ -6,15 +6,27 @@ int CALLBACK WinMain(
 	_In_ LPSTR     lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	Window wnd(800, 600, L"Donkey Fart Box");
-	MSG msg;
-	BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+	try {
+		Window wnd(800, 600, L"Donkey Fart Box");
+		MSG msg;
+		BOOL gResult;
+		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		if (gResult == -1) {
+			return -1;
+		}
+		return msg.wParam;
 	}
-	if (gResult == -1) {
-		return -1;
+	catch (const CustomException& e) {
+		MessageBox(nullptr, (LPCWSTR)e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
 	}
-	return msg.wParam;
+	catch (const std::exception& e) {
+		MessageBox(nullptr, (LPCWSTR)e.what(), L"Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...) {
+		MessageBox(nullptr, L"No details available", L"Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;
 }
