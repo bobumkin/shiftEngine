16165c1110cafd930955ca2266263a31a95b7c17
bobumkin <112267231+bobumkin@users.noreply.github.com>
2024-05-20 18:42:44 +0500

    Added dynamic lighting
    Added dynamic lighting for realistic 3D graphics

--- App.h
+++ App.h
@@ -3,21 +3,31 @@
 #include "Timer.h"
 #include "ImguiManager.h"
 #include "Camera.h"
+#include "PointLight.h"
+#include <set>
 
 class App
 {
 public:
 	App();
+	// master frame / message loop
 	int Go();
 	~App();
 private:
 	void DoFrame();
+	void SpawnSimulationWindow() noexcept;
+	void SpawnBoxWindowManagerWindow() noexcept;
+	void SpawnBoxWindows() noexcept;
 private:
 	ImguiManager imgui;
 	Window wnd;
 	Timer timer;
 	std::vector<std::unique_ptr<class Drawable>> drawables;
+	std::vector<class Box*> boxes;
 	float speed_factor = 1.0f;
 	Camera cam;
+	PointLight light;
 	static constexpr size_t nDrawables = 180;
+	std::optional<int> comboBoxIndex;
+	std::set<int> boxControlIds;
 };
