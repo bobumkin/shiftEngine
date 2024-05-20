12b1cd8922e87ff12f37fde7c7d16244bfe91f19
bobumkin <112267231+bobumkin@users.noreply.github.com>
2024-05-20 18:43:40 +0500

    Added dynamic lighting #2
    Added dynamic lighting for more realistic 3D graphics.

--- IndexedTriangleList.h
+++ IndexedTriangleList.h
@@ -2,7 +2,7 @@
 #include <vector>
 #include <DirectXMath.h>
 
-template <class T>
+template<class T>
 class IndexedTriangleList
 {
 public:
@@ -15,9 +15,10 @@ public:
 		assert(vertices.size() > 2);
 		assert(indices.size() % 3 == 0);
 	}
-	void Transform(DirectX::XMMATRIX matrix)
+	void Transform(DirectX::FXMMATRIX matrix)
 	{
-		for (auto& v : vertices) {
+		for (auto& v : vertices)
+		{
 			const DirectX::XMVECTOR pos = DirectX::XMLoadFloat3(&v.pos);
 			DirectX::XMStoreFloat3(
 				&v.pos,
@@ -25,6 +26,28 @@ public:
 			);
 		}
 	}
+	// asserts face-independent vertices w/ normals cleared to zero
+	void SetNormalsIndependentFlat() noexcept(!IS_DEBUG)
+	{
+		using namespace DirectX;
+		assert(indices.size() % 3 == 0 && indices.size() > 0);
+		for (size_t i = 0; i < indices.size(); i += 3)
+		{
+			auto& v0 = vertices[indices[i]];
+			auto& v1 = vertices[indices[i + 1]];
+			auto& v2 = vertices[indices[i + 2]];
+			const auto p0 = XMLoadFloat3(&v0.pos);
+			const auto p1 = XMLoadFloat3(&v1.pos);
+			const auto p2 = XMLoadFloat3(&v2.pos);
+
+			const auto n = XMVector3Normalize(XMVector3Cross((p1 - p0), (p2 - p0)));
+
+			XMStoreFloat3(&v0.n, n);
+			XMStoreFloat3(&v1.n, n);
+			XMStoreFloat3(&v2.n, n);
+		}
+	}
+
 public:
 	std::vector<T> vertices;
 	std::vector<unsigned short> indices;
